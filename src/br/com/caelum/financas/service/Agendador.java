548744b0753f653ec2bcd32a855fab7b947cfee2
jpa7145 <jpa7145@caelum133-12>
2017-11-10 20:48:07 -0200

    metade da aula timerservice.

--- src/br/com/caelum/financas/service/Agendador.java
+++ src/br/com/caelum/financas/service/Agendador.java
@@ -2,14 +2,50 @@ package br.com.caelum.financas.service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.ScheduleExpression;
 import javax.ejb.Singleton;
-import javax.ejb.Stateless;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
 
 //@Stateless
 @Singleton
 public class Agendador {
 
 	private static int totalCriado;
+	
+	@Resource
+	private TimerService timerService;
+	
+	public void agenda(String expressaoMinutos, String expressaoSegundos){
+		
+		ScheduleExpression expression = new ScheduleExpression();
+		
+		expression.hour("*");
+		expression.minute(expressaoMinutos);
+		expression.second(expressaoSegundos);
+		
+		TimerConfig config = new TimerConfig();
+		
+		config.setInfo(expression.toString());
+		config.setPersistent(false);
+
+		
+		this.timerService.createCalendarTimer(expression, config);
+		
+		System.out.println("Agendamento: " + expression);
+		
+		
+	}
+	
+	@Timeout
+	public void verificacaoPeriodicaSeHaNovasContas(Timer timer){
+		
+		System.out.println(timer.getInfo());
+		
+	}
 
 	public void executa() {
 		System.out.printf("%d instancias criadas %n", totalCriado);
